5aa9f2b06e96fc1cd83485913c7179b8b79a81bd
nomishpriv <114777958+nomishpriv@users.noreply.github.com>
2023-06-26 02:58:56 +0500

    2nd Update db.js

--- src/confiq/db.js
+++ src/confiq/db.js
@@ -4,7 +4,7 @@ const { error } = require("../helper/ErrorHandler");
 const DATABASE_URL = 'mongodb://mongo:rjoJNpEL0fMvLQWEHzno@containers-us-west-35.railway.app:6846'
 const connectDB = async (DATABASE_URL) => {
   try {
-    await mongoose.connect(DATABASE_URL);
+    await mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log(DATABASE_URL + " DB Connected Successfully...");
   } catch (error) {
     console.log(error);
