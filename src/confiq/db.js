824e0c042cf1bff237fb2c4a62ecb629c695df9b
nomishpriv <114777958+nomishpriv@users.noreply.github.com>
2023-06-27 22:26:15 +0500

    revert db.js

--- src/confiq/db.js
+++ src/confiq/db.js
@@ -1,14 +1,14 @@
 //External Lib Import
 const mongoose = require("mongoose");
 const { error } = require("../helper/ErrorHandler");
-const DATABASE_URL = 'mongodb://mongo:rjoJNpEL0fMvLQWEHzno@containers-us-west-35.railway.app:6846'
-const connectDB = async (DATABASE_URL) => {
+
+const connectDB = async (DATABASE_URL, DB_OPTIONS) => {
   try {
-    await mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
-    console.log(DATABASE_URL + " DB Connected Successfully...");
+    await mongoose.connect(DATABASE_URL, DB_OPTIONS);
+    console.log(DB_OPTIONS.dbName + " DB Connected Successfully...");
   } catch (error) {
     console.log(error);
-    console.log(DATABASE_URL + " DB Connected Failure...");
+    console.log(DB_OPTIONS.dbName + " DB Connected Failure...");
   }
 };
 
